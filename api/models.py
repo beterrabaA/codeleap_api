f497f8a867818cc812bd5ed0617bebe6a45e60a1
Beterraba <ls6182315@gmail.com>
2025-05-07 21:11:53 -0300

    refactor Post model: remove redundant id field and update string representation

--- api/models.py
+++ api/models.py
@@ -2,13 +2,13 @@ import datetime
 
 from django.db import models
 
+
 # Create your models here.
 class Post(models.Model):
-    id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=100)
+    created_datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=255)
-    created_datetime = models.DateTimeField()
 
     def __str__(self):
-        return self.username
+        return f"{self.username} - {self.title}"
