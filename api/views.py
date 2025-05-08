d2d44379f150d7b33451e56fbeee21054b75b457
Beterraba <ls6182315@gmail.com>
2025-05-07 21:21:58 -0300

    refactor views: implement posts and posts_by_id functions for handling GET, POST, PATCH, and DELETE requests

--- api/views.py
+++ api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializer import PostSerializer, PostCreateSerializer, PostUpdateSerializer
+
+
+@api_view(["GET", "POST"])
+def posts(request):
+    if request.method == "GET":
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = PostCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PATCH", "DELETE"])
+def posts_by_id(request, post_id):
+    if request.method == "PATCH":
+        post = Post.objects.get(id=post_id)
+        serializer = PostUpdateSerializer(
+            post,
+            data=request.data,
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "DELETE":
+        post = Post.objects.get(id=post_id)
+        post.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
