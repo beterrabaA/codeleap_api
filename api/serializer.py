b35ebd06ca905d81e3dd440ca2e2b726ac846a0c
Beterraba <ls6182315@gmail.com>
2025-05-07 21:14:06 -0300

    refactor serializers: enhance PostCreateSerializer and PostUpdateSerializer with required fields and read-only settings

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,34 @@
 from rest_framework import serializers
 from .models import Post
 
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = "__all__"
+        read_only_fields = ["id", "created_datetime"]
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["username", "title", "content"]
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "created_datetime": {"read_only": True},
+            "username": {"required": True},
+            "title": {"required": True},
+            "content": {"required": True},
+        }
+        read_only_fields = ["created_datetime"]
+
+
+class PostUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["title", "content"]
+        extra_kwargs = {
+            "title": {"required": True},
+            "content": {"required": True},
+        }
+        read_only_fields = ["created_datetime"]
