fff5f7ba7e474c15d8626774ad03b7e8cb0f2761
Beterraba <ls6182315@gmail.com>
2025-05-07 22:49:04 -0300

    fix: update settings to load environment variables and adjust database configuration

--- codeleap_app/settings.py
+++ codeleap_app/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -78,11 +79,11 @@ WSGI_APPLICATION = "codeleap_app.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "codeleap_db"),
-        "USER": os.getenv("POSTGRES_USER", "codeleap_user"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "codeleap_password"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
